c78f629b1c1ebf42bcf83867944b969cf112d6c8
Gijs Lamon <info@gileba.be>
2021-03-29 11:06:05 +0200

    Add suipport for custom logo

--- src/functions.php
+++ src/functions.php
@@ -1,12 +1,33 @@
 <?php
 
-function register_my_menus() {
-  register_nav_menus(
-	array(
-	  'header-menu' => __( 'Header Menu' )
-	 )
-   );
- }
- add_action( 'init', 'register_my_menus' );
+function musalie_register_menus() {
+	register_nav_menus(
+		array(
+			'header-menu' => __( 'Header Menu' )
+		)
+	);
+}
  
- ?>
+/**
+ * Add support for core custom logo.
+ *
+ * @link https://codex.wordpress.org/Theme_Logo
+ */
+add_theme_support(
+	'custom-logo',
+	array(
+		'height'               	=> 300,
+		'width'                	=> 100,
+		'flex-width'           	=> true,
+		'flex-height'          	=> true,
+		'header-text'			=> array( 'site-title', 'site-description' ),
+		'unlink-homepage-logo' 	=> true,
+	)
+);
+
+// Add support for Block Styles.
+add_theme_support( 'wp-block-styles' );
+
+add_action( 'init', 'musalie_register_menus' );
+
+?>
--- src/header.php
+++ src/header.php
@@ -5,8 +5,17 @@
 	</head>
 	
 	<body>
-		<div id="musalie-site">
-			<h1><?php echo esc_html(get_bloginfo( 'name' )); ?></h1>
+		<div id="musalie-header">
+		<?php
+			$custom_logo_id = get_theme_mod( 'custom_logo' );
+			$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+
+			if ( has_custom_logo() ) {
+				echo '<img src="' . esc_url( $logo[0] ) . '" alt="' . get_bloginfo( 'name' ) . '">';
+			} else {
+				echo '<h1>' . get_bloginfo('name') . '</h1>';
+			}
+		?>
 			<nav><?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?></nav>
 		</div>
 		<div id="musalie-container">
