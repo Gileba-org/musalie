2db1cde19229ce00d0dccb082b17576f45638fea
Gijs Lamon <info@gileba.be>
2021-03-29 23:24:25 +0200

    Title should be H1

--- src/index.php
+++ src/index.php
@@ -4,10 +4,10 @@
 	<div id="musalie_content">
 		<div class="row">
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<?php if (!is_home()) { ?>
+				<?php the_title('<h1 class="post-title">', '</h1>'); ?>
+			<?php } ?>
 			<div class="post">
-				<?php if (!is_home()) { ?>
-					<?php the_title('<h2>', '</h2>'); ?>
-				<?php } ?>
 				<p><?php the_content(__('(more...)')); ?></p>
 			</div>
 		<?php endwhile; else: ?>
