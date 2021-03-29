542a1a64bc55f2618068d959943e9e9257545c3f
Gijs Lamon <info@gileba.be>
2021-03-29 08:03:06 +0000

    Basic template

---
+++ src/header.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+	<title><?php echo esc_html(get_bloginfo( 'name' )); ?></title>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+	</head>
+	
+	<body>
+		<div id="musalie-site">
+			<h1><?php echo esc_html(get_bloginfo( 'name' )); ?></h1>
+		</div>
+		<div id="musalie-container">
---
+++ src/index.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<div id="musalie_main">
+	<div id="musalie_content">
+		<div class="row">
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<div class="post">
+				<?php if (!is_home()) { ?>
+					<?php the_title('<h2>', '</h2>'); ?>
+				<?php } ?>
+				<p><?php the_content(__('(more...)')); ?></p>
+			</div>
+		<?php endwhile; else: ?>
+			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+		<?php endif; ?>
+		</div>
+	</div>
+</div>
